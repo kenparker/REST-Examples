5285c7140a47317e27a67f12d5c0636ef0944efc
Angelo <magangde@yahoo.de>
2014-10-10 11:28:59 +0200

    Test POST when stock is already there added (Test result is internal error) / Test GET stock found added

--- TradingCore/TradingCore-Client/src/test/java/com/maggioni/Test/StocksRESTClient.java
+++ TradingCore/TradingCore-Client/src/test/java/com/maggioni/Test/StocksRESTClient.java
@@ -1,61 +1,89 @@
 package com.maggioni.Test;
 
-
-
 import com.maggioni.Entities.Stock;
 import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-
-
-public class StocksRESTClient
-{
+public class StocksRESTClient {
 
     private WebTarget resource;
     private Client client;
     private static final String BASE_URI = "http://localhost:8080/TradingCore-Server/resources/";
 
-    public StocksRESTClient()
-    {
+    public StocksRESTClient() {
+
+    }
+
+    @Before
+    public void setUp() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
         resource = client.target(BASE_URI).path("stocks");
     }
 
     @Test
-    public void createStock() throws ClientErrorException
-    {
-        System.out.println("*** Create a new Stock *****");
-       
-        Stock st = new Stock("SPY","fff");
+    public void create_stock_Found_error() throws ClientErrorException {
+        System.out.println("*** Create a new Stock - Stock found *****");
+
+        Stock st = new Stock("QQQ", "Nasdaq");
         Response response = resource.request()
                 .post(Entity.entity(st, MediaType.APPLICATION_XML));
-        if (response.getStatus() != 201)
-        {
-            System.out.println("Error");
-        }
+       
+        Assert.assertEquals(Status.fromStatusCode(response.getStatus()), Status.INTERNAL_SERVER_ERROR);
+    }
 
-        String location = response.getLocation().toString();
-        System.out.println("location: " + location);
+    @Test
+    public void create_stock_created() throws ClientErrorException {
+        System.out.println("*** Create a new Stock - Stock created *****");
+
+        Stock st = new Stock("QQQ3", "Nasdaq");
+        Response response = resource.request()
+                .post(Entity.entity(st, MediaType.APPLICATION_XML));
+        
+        if (Status.fromStatusCode(response.getStatus()) == Status.CREATED) {
+            String location = response.getLocation().toString();
+            System.out.println("location: " + location);
+        }
+        Assert.assertEquals(Status.CREATED,Status.fromStatusCode(response.getStatus()));
+    }
+
+    @Test
+    public void get_stock_found() {
+        System.out.println("*** get Stock - stock found ****");
+        
+        String symbol = "SPY";
+        
+        Builder request = resource.path("/{symbol}")
+                .resolveTemplate("symbol", symbol)
+                .request();      
+        Response response = request.get();
+        
+        Assert.assertEquals(Status.OK,Status.fromStatusCode(response.getStatus()));
+        Stock stock = request.get(Stock.class);
+        Assert.assertEquals(symbol,stock.getSymbol());
         
     }
-   
     /*
-    public void getStock(String location)
-    {
-        System.out.println("*** GET Stock ***");
-        //final WebTarget path = client.target(BASE_URI+location);
-        //System.out.println(path.getUri().toString());
-        String stock = client.target(location).request().get(String.class);
-        System.out.println(stock);
-    }
-*/
-    public void close()
-    {
+     public void getStock(String location)
+     {
+     System.out.println("*** GET Stock ***");
+     //final WebTarget path = client.target(BASE_URI+location);
+     //System.out.println(path.getUri().toString());
+     String stock = client.target(location).request().get(String.class);
+     System.out.println(stock);
+     }
+     */
+    @After
+    public void close() {
         client.close();
     }
 }

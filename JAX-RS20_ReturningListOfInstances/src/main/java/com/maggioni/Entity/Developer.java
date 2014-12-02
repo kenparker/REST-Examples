10be6001f3b59f981db19226ae58cf32b0c40408
Angelo <mail@amaggioni.com>
2014-12-02 17:41:58 +0100

    JAX-RS20

---
+++ JAX-RS20_ReturningListOfInstances/src/main/java/com/maggioni/Entity/Developer.java
@@ -0,0 +1,36 @@
+
+package com.maggioni.Entity;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author maggioni
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
+public class Developer {
+    
+    @XmlElement(name = "firstname")
+    private String name;
+    private String last;
+
+    public Developer() {
+        
+    }
+    public Developer(String name, String last) {
+        this.name = name;
+        this.last = last;
+    }
+
+    @Override
+    public String toString() {
+        return "Developer{" + "name=" + name + ", last=" + last + '}';
+    }
+    
+    
+    
+}
